2e47b51e5acc7f0ae16da706b0aa0cee30e62bdd
Sander Viirmaa <h4n1virus@gmail.com>
2019-07-12 03:13:31 +0300

    Update admin.py

--- merchant/modules/admin.py
+++ merchant/modules/admin.py
@@ -7,8 +7,6 @@ from pyrogram import Filters, Message
 
 from merchant import BOT, ADMINS
 
-loop = asyncio.get_running_loop()
-
 
 @BOT.on_message(Filters.user(users=ADMINS) & Filters.command('update', '!'))
 async def pull_update():
@@ -30,6 +28,7 @@ async def up(bot: BOT, message: Message):
 async def ping(bot:  BOT, message: Message):
     print(message.command)
     ip = message.command[1]
+    loop = asyncio.get_running_loop()
     proc = await loop.create_subprocess_shell(
         'ping {} -c 4'.format(ip),
         stdout=loop.subprocess.PIPE,
