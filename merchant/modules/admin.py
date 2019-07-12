ff9877db429c060c6806d0cea385e8be39a3a9ff
Sander Viirmaa <h4n1virus@gmail.com>
2019-07-12 03:46:03 +0300

    removed test

--- merchant/modules/admin.py
+++ merchant/modules/admin.py
@@ -1,5 +1,4 @@
 import os
-import asyncio
 import subprocess
 import sys
 
@@ -17,29 +16,3 @@ async def pull_update():
 @BOT.on_message(Filters.user(users=ADMINS) & Filters.command('restart', '!'))
 async def restart():
     os.execl(sys.executable, sys.executable, *sys.argv)
-
-
-@BOT.on_message(Filters.command('up', '!'))
-async def up(bot: BOT, message: Message):
-    await message.reply("I am alive master")
-
-
-@BOT.on_message(Filters.command('bash', '!') & Filters.user(users=ADMINS))
-async def sh(bot:  BOT, message: Message):
-    cmd = ' '.join(message.command[1:])
-    print(cmd)
-
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
-
-    stdout, stderr = await proc.communicate()
-    stdout = stdout.decode()
-    stderr = stderr.decode()
-
-    if stdout:
-        await message.reply(stdout)
-    elif stderr:
-        await message.reply(stderr)
